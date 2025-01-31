176c14b929e00961a653c3d3a6df542f1408bb06
suman043 <suman.mohanty043@gmail.com>
2025-01-31 16:41:21 +0530

    Session id added

--- src/main/java/com/springSecurityTemplate/controller/HelloController.java
+++ src/main/java/com/springSecurityTemplate/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.springSecurityTemplate.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String greet(){
-        return "Hello World";
+    public String greet(HttpServletRequest httpServletRequest){
+        return "Hello World " + httpServletRequest.getSession().getId();
     }
 
     @GetMapping("/about")
-    public String about(){
-        return "Demo Security";
+    public String about(HttpServletRequest httpServletRequest){
+        return "Demo Security " + httpServletRequest.getSession().getId();
     }
 }
