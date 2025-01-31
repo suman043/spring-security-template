51a8d489cd55173b261bccebbe0daa3b54d45279
suman043 <suman.mohanty043@gmail.com>
2025-01-31 15:46:15 +0530

    Initial Commit

---
+++ src/test/java/com/springSecurityTemplate/SpringSecurityTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springSecurityTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
