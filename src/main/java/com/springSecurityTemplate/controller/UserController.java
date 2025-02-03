eebc5c73179ed237c78e69fac804a5d5f72e5fe0
suman043 <suman.mohanty043@gmail.com>
2025-02-03 13:36:39 +0530

    Adding users in database

---
+++ src/main/java/com/springSecurityTemplate/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.springSecurityTemplate.controller;
+
+import com.springSecurityTemplate.model.User;
+import com.springSecurityTemplate.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        return userService.saveUser(user);
+    }
+}
---
+++ src/main/java/com/springSecurityTemplate/service/UserService.java
@@ -0,0 +1,18 @@
+package com.springSecurityTemplate.service;
+
+import com.springSecurityTemplate.model.User;
+import com.springSecurityTemplate.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+}
