9b43225f770af5b90e793c67f04070076ae897a9
suman043 <suman.mohanty043@gmail.com>
2025-02-03 16:56:33 +0530

    Final Spring Security with password encryption

--- src/main/java/com/springSecurityTemplate/config/SecurityConfig.java
+++ src/main/java/com/springSecurityTemplate/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,7 +29,8 @@ public class SecurityConfig {
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        //daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         return daoAuthenticationProvider;
     }
 
--- src/main/java/com/springSecurityTemplate/service/UserService.java
+++ src/main/java/com/springSecurityTemplate/service/UserService.java
@@ -3,6 +3,7 @@ package com.springSecurityTemplate.service;
 import com.springSecurityTemplate.model.User;
 import com.springSecurityTemplate.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,12 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+
     public User saveUser(User user){
+
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        System.out.println(user.getPassword());
         return userRepository.save(user);
     }
 
